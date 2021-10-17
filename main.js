648cb3133078b8bb15739932230e8421e2d8d501
DavidCDCB <cristian-cb1@hotmail.com>
2021-10-17 12:19:16 -0500

    Mejora_Gráficos

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ var app = new Vue({
 	data: {
 		data: [],
 		estado: {},
+		colores: {
+			7:'#A5D788',
+			6:'#38A8A1',
+			5:'#3777A4',
+			4:'#4537A4',
+			3:'#541C38',
+			2:'#260D1E',
+			1:'#09050F'
+		}
 	},
 	methods: {
 		async downData() {
@@ -36,9 +45,38 @@ var app = new Vue({
 				});
 			}
 		},
+		getColorProm(value){
+			if(value > 7){
+				return this.colores[7];
+			}
+			if(value > 6){
+				return this.colores[6];
+			}
+			if(value > 5){
+				return this.colores[5];
+			}
+			if(value > 4){
+				return this.colores[4];
+			}
+			if(value > 3){
+				return this.colores[3];
+			}
+			if(value > 2){
+				return this.colores[2];
+			}
+			if(value > 1){
+				return this.colores[1];
+			}
+		},
+
 		getdata() {
 			let labels = [];
 			let levels = [];
+			let listColors = [];
+			let promedios = [];
+			let sumatoria = 0;
+			let tagsProm = [];
+			let colorsProm = [];
 			let conteos = {
 				"Alegre":0,
 				"Excelente":0,
@@ -49,32 +87,42 @@ var app = new Vue({
 				"Una Mierda":0
 			};
 
+
 			this.downData().then(r => {
-				console.log(Object.values(r.data));
 				this.data = Object.values(r.data);
 				for (const iterator of this.data) {
-					console.log(iterator.Fecha);
 					labels.push(iterator.Fecha);
 					levels.push(iterator.Nivel);
 					conteos[iterator.Estado] += 1;
+					listColors.push(this.colores[iterator.Nivel]);
+					sumatoria += iterator.Nivel;
+
+					if(levels.length % 15 == 0){
+						promedios.push((sumatoria/levels.length));
+						tagsProm.push(iterator.Fecha);
+						colorsProm.push(this.getColorProm(sumatoria/levels.length));
+					}
+					
 				}
 
-				console.log(conteos);
+				console.log(promedios);
 
 				const data = {
 					labels: labels,
 					datasets: [{
 						label: 'Estados de ánimo',
-						backgroundColor: '#063346',
-						borderColor: '#063346',
+						backgroundColor: listColors,
 						data: levels,
 					}]
 				};
 				const config = {
-					type: 'line',
+					type: 'bar',
 					data: data,
 					options: {
 						responsive: true,
+						layout: {
+							padding: 20
+						}
 					}
 				};
 				var myChart = new Chart(
@@ -88,31 +136,54 @@ var app = new Vue({
 					datasets: [{
 						label: 'Conteo por estado',
 						data: Object.values(conteos),
-						backgroundColor: [
-							'#A5D788',
-							'#38A8A1',
-							'#3777A4',
-							'#4537A4',
-							'#541C38',
-							'#260D1E',
-							'#09050F'
-						]
+						backgroundColor: Object.values(this.colores).reverse()
 					}]
 				};
 				const config2 = {
 					type: 'pie',
 					data: data2,
 					options: {
-						scales: {
-							y: {
-								beginAtZero: true
-							}
+						layout: {
+							padding: 100
 						}
 					},
 				};
 				var myChart2 = new Chart(
 					document.getElementById('cantidades'),
 					config2
+				);
+
+				const data3 = {
+					labels: tagsProm,
+					datasets: [{
+						label: 'Cambio en promedio',
+						backgroundColor: '#063346',
+						borderColor: '#282c34',
+						borderWidth: 1,
+						pointRadius: 5,
+						data: promedios,
+						backgroundColor: colorsProm
+					}]
+				};
+				const config3 = {
+					type: 'line',
+					data: data3,
+					options:  {
+						layout: {
+							padding: 20
+						},
+						scales: {
+							y: {
+								scaleOverride : true,
+								scaleStartValue : 0
+							}
+						}
+					},
+				};
+
+				var myChart3 = new Chart(
+					document.getElementById('promedios'),
+					config3
 				);
 			});
 		}
