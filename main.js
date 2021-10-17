b74ea4399865069f772ab5eeb7d7642e468daf7c
DavidCDCB <cristian-cb1@hotmail.com>
2021-10-16 21:40:13 -0500

    Gráfico

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 var app = new Vue({
 	el: '#app',
-	mounted(){
-	
+	mounted() {
+
 	},
 	data: {
 		data: [],
 		estado: {},
 	},
-	methods:{		
-		async downData(){
-			return await axios.get('https://bdethos-default-rtdb.firebaseio.com/clients.json');
+	methods: {
+		async downData() {
+			return await axios.get('https://bdethos-default-rtdb.firebaseio.com/status.json');
 		},
-
-		upData(level,name){
+		upData(level, name) {
 			this.estado = {
 				"Estado": name,
 				"Nivel": level,
 				"Fecha": new Date().toDateString()
 			}
-			if(this.estado!=null){
+			if (this.estado != null) {
 				axios.post('https://bdethos-default-rtdb.firebaseio.com/status.json',
-				this.estado,{
-				headers:{
-					'Content-Type': 'application/json'
-				}}
+					this.estado, {
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
 				).then(response => {
 					console.log(response);
 					swal({
@@ -34,8 +34,40 @@ var app = new Vue({
 						dangerMode: false
 					});
 				});
-			} 
+			}
 		},
-
+		getdata() {
+			let labels = [];
+			let levels = [];
+			this.downData().then(r => {
+				console.log(Object.values(r.data));
+				this.data = Object.values(r.data);
+				for (const iterator of this.data) {
+					console.log(iterator.Fecha);
+					labels.push(iterator.Fecha);
+					levels.push(iterator.Nivel);
+				}
+				const data = {
+					labels: labels,
+					datasets: [{
+						label: 'Estados de ánimo',
+						backgroundColor: '#063346',
+						borderColor: '#063346',
+						data: levels,
+					}]
+				};
+				const config = {
+					type: 'line',
+					data: data,
+					options: {
+						responsive: true,
+					}
+				};
+				var myChart = new Chart(
+					document.getElementById('estados'),
+					config
+				);
+			});
+		}
 	}
 });
